35fc3a0963515d1041166e455e40e51fa38c37bf
teezcode <spaschal43@gmail.com>
2025-07-22 02:03:09 +0100

    worked on sending data to db

---
+++ todo/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Task
+
+def addTask(request):
+    task = (request.POST['task'])
+    Task.objects.create(task=task)
+    return redirect('home')
